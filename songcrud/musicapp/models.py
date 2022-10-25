f28dfce6df1cb873722a6c4338decc47fa3fa1ad
Ibeetson2 <yinka4real05@yahoo.com>
2022-10-25 22:04:15 +0100

    second commit

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField()
+
+class Song(models.Model):
+    Artiste = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+    title = models.CharField(max_length=20)
+    date_released = models.DateField(default = datetime.date.today)
+    likes =  models.IntegerField()
+    artist_id =  models.IntegerField()
+
+class Lyrics(models.Model):
+    content = models.ForeignKey(Song, on_delete=models.CASCADE)
+    song_id =  models.IntegerField()
